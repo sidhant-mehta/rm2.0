b4108916f470b3671fdec375c2a4afd16ea22b5d
Sidhant Mehta <sidhant_mehta@yahoo.com>
2013-04-11 14:34:23 +0100

    updated homepage in root and removed public index.html file

--- config/routes.rb
+++ config/routes.rb
@@ -137,8 +137,7 @@ RaisonMentors20::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   
-  # TODO need to change the root to point to a home page
-  root :to => 'mentors#index'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
