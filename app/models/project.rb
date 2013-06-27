0d6d9184f89061785cd3596174ebba182e25484e
Sidhant Mehta <sidhant_mehta@yahoo.com>
2013-06-27 23:55:33 +0100

    the project search is now working well.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,16 @@
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
 class Project < ActiveRecord::Base
-  attr_accessible :closing_date, :description, :draft, :email, :location, :name, :organisation, :project_leader, :sector_ids, :telephone
+  attr_accessible :closing_date, :description, :draft, :email, :location, :name, :organisation, :project_leader, :sector_ids, :telephone, :salary
+
+#TODO add validation - general - salary
 
 
-    def self.search (name, sector, closing_date, pay)
+    def self.search (name, sector, closing_date, salary)
       t = Project.arel_table
 
       if (closing_date.nil?) 
@@ -12,11 +20,17 @@ class Project < ActiveRecord::Base
       d, m, y = closing_date.split '-' #need to check if date is valid otherwise tries to perform date operation on invalid input - error.
       if (Date.valid_date? y.to_i, m.to_i, d.to_i)
          
-          return Project.where(t[:name].matches("%#{name}%").and((t[:sector_ids].matches("%#{sector}%")))).
+          result = Project.where(t[:name].matches("%#{name}%").and((t[:sector_ids].matches("%#{sector}%")))).
                 where("closing_date > ?", Date.strptime(closing_date,"%d-%m-%Y"))
       else
-          return Project.where(t[:name].matches("%#{name}%").and((t[:sector_ids].matches("%#{sector}%"))))
+          result = Project.where(t[:name].matches("%#{name}%").and((t[:sector_ids].matches("%#{sector}%"))))
       end
-  
+      
+      if (salary.is_number? ==true) #if it is a number then you need to add another condition to have salary at least that much. If it is not a number (so "Expenses") then you can include all results hence no need to add a condition. If someone enteres another non-number except for "Expenses" same idea applies (it is not a number so show all)
+          result = result.where("salary <> ? and salary > ?", "Expenses", salary) #needed to add the <> condition because with just > it still shows the "Expenses" results
+      end
+        
+      return result
+        
      end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130627201903) do
+ActiveRecord::Schema.define(:version => 20130627205026) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -179,6 +179,7 @@ ActiveRecord::Schema.define(:version => 20130627201903) do
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
     t.string   "image"
+    t.string   "salary"
   end
 
   create_table "sectors", :force => true do |t|
