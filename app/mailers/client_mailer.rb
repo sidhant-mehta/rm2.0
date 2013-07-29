bddc3650ecaa1b8859e9c25d65cb4701e78d1842
Sidhant Mehta <sidhant_mehta@yahoo.com>
2013-07-29 17:29:24 +0100

    devise mailer is now working. when client edits their email address they will get confirmation email before they can use their new email to login. currently using gmail account settings

---
+++ app/mailers/client_mailer.rb
@@ -0,0 +1,3 @@
+class ClientMailer < ActionMailer::Base
+  default from: "sidhant.mehta01@gmail.com"
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130729142551) do
+ActiveRecord::Schema.define(:version => 20130729155712) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -75,8 +75,12 @@ ActiveRecord::Schema.define(:version => 20130729142551) do
     t.datetime "updated_at",                             :null => false
     t.string   "organisation"
     t.string   "unconfirmed_email"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "clients", ["confirmation_token"], :name => "index_clients_on_confirmation_token", :unique => true
   add_index "clients", ["email"], :name => "index_clients_on_email", :unique => true
   add_index "clients", ["reset_password_token"], :name => "index_clients_on_reset_password_token", :unique => true
 
