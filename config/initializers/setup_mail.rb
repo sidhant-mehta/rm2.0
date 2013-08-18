8b928a3783a94b0ed7dd8016d942afb3b5cf5981
Sidhant Mehta <sidhant_mehta@yahoo.com>
2013-08-18 20:57:20 +0100

    pre pull

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -6,7 +6,7 @@ ActionMailer::Base.smtp_settings = {
   :port                 => 587,
   :domain               => "gmail.com",
   :user_name            => "sidhant.mehta01",
-  :password             => "JaiGanesh19",
+  :password             => "",
   :authentication       => "plain",
   :enable_starttls_auto => true
 
