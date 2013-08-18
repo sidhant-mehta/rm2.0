884e9c4c9c604c69af1d08f0be7b65f908b0b2b5
sidhant-mehta <sidhant_mehta@yahoo.com>
2013-08-18 20:52:25 +0100

    Update setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -5,8 +5,8 @@ ActionMailer::Base.smtp_settings = {
 :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "gmail.com",
-  :user_name            => "sidhant.mehta01",
-  :password             => "JaiGanesh19",
+  :user_name            => "***",
+  :password             => "***",
   :authentication       => "plain",
   :enable_starttls_auto => true
 
